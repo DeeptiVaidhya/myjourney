fbad8ab1643a8e12b67d85578a2aec8027baafc8
Rohan Verma <rohan.verma@ideavate.com>
2019-07-08 18:08:47 +0530

    Questionnaire validation changes.

--- src/app/components/questionnaire/questionnaire.component.ts
+++ src/app/components/questionnaire/questionnaire.component.ts
@@ -1,102 +1,146 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Router } from '@angular/router';
-import { ToastrService } from 'ngx-toastr';
-import { QuestionnaireService } from '../../service/questionnaire.service';
-
+import { Component, OnInit, TemplateRef } from "@angular/core";
+import { ActivatedRoute, Router } from "@angular/router";
+import { BsModalRef, BsModalService } from "ngx-bootstrap/modal";
+import { ToastrService } from "ngx-toastr";
+import { QuestionnaireService } from "../../service/questionnaire.service";
 @Component({
-	selector: 'app-questionnaire',
-	templateUrl: './questionnaire.component.html',
-	styleUrls: ['./questionnaire.component.css']
+    selector: "app-questionnaire",
+    templateUrl: "./questionnaire.component.html",
+    styleUrls: ["./questionnaire.component.css"]
 })
 export class QuestionnaireComponent implements OnInit {
+    modalRef: BsModalRef;
+    data: any;
+    options = [];
+    questions: any;
+    answer: any;
+    group_title: any;
+    short_description: any;
+    total_question_groups = 0;
+    page_number = 1;
+    type: string = "";
+    symptom_percentage = 0;
+    totalQuestions: number = 0;
+    weekInfoId: any;
+    confirm_submition: boolean = false;
+    question_count = 0;
+    breadcrumb = [
+        { link: "/", title: "Home" },
+        { title: "Questionnaire", class: "active" }
+    ];
 
-	data: any;
-	options = [];
-	questions: any;
-	answer: any;
-	group_title: any;
-	short_description: any;
-	total_question_groups = 0;
-	page_number = 1;
-	type: string = '';
-	symptom_percentage = 0;
-	weekInfoId: any;
-	question_count = 0;
-	breadcrumb = [{ link: '/', title: 'Home' }, { title: 'Questionnaire', class: 'active' }];
+    constructor(
+        private router: Router,
+        private route: ActivatedRoute,
+        public toastr: ToastrService,
+        public questionnaireService: QuestionnaireService,
+        private modalService: BsModalService
+    ) {}
+    /**
+     * @desc Set all Pro-Ctcae Questions on Page View Init
+     */
+    ngOnInit() {
+        this.route.params.subscribe(param => {
+            this.type =
+                param["type"] == "case-cancer" ? "CASE_CANCER" : param["type"];
+            // this.question = [];
+            console.log(this.type);
+            this.getQuestionnaire();
+        });
+    }
 
-	constructor(
-		private router: Router,
-		private route: ActivatedRoute,
-		public toastr: ToastrService,
-		public questionnaireService: QuestionnaireService
-	) { }
-	/**
-	 * @desc Set all Pro-Ctcae Questions on Page View Init
-	 */
-	ngOnInit() {
+    /**
+     * @desc Get Pro-Ctcae Questionnaire for each patient depending on cancer type.
+     */
+    getQuestionnaire() {
+        this.questionnaireService
+            .get_questionnaire({ type: this.type.toUpperCase() })
+            .subscribe(Response => {
+                if (Response["status"] == "success") {
+                    this.questions = Response["questions"];
+                    this.totalQuestions = this.questions.length;
+                    console.log(this.totalQuestions);
+                    this.options = Response["options"];
+                    this.group_title = Response["group_title"];
+                    this.weekInfoId = Response["week_info"]["id"];
+                    this.short_description = Response["short_description"];
+                } else {
+                    this.toastr.error(Response["msg"]);
+                    this.router.navigate(["/patient/dashboard"]);
+                }
+            });
+    }
 
-		this.route.params.subscribe(param => {
-			this.type = param['type'] == 'case-cancer' ? 'CASE_CANCER' : param['type'];
-			// this.question = [];
-			console.log(this.type);
-			this.getQuestionnaire();
-		});
+    openModal(template: TemplateRef<any>) {
+        this.modalRef = this.modalService.show(template);
+    }
 
-	}
+    confirmSubmission() {
+        this.confirm_submition = true;
+        this.save();
+    }
 
-	/**
-	 * @desc Get Pro-Ctcae Questionnaire for each patient depending on cancer type.
-	 */
-	getQuestionnaire() {
+    highlightQuestions() {
+        this.modalRef.hide();
+        let tr = document.querySelectorAll("tr.quest-row");
+        for (let i = 0, l = tr.length; i < l; i++) {
+            !tr[i].querySelector("input[name^='option[']:checked") &&
+                ((tr[i].querySelector(
+                    ".c-purple"
+                ) as HTMLTableColElement).style.color = "#FF0000");
+        }
+    }
 
-		this.questionnaireService.get_questionnaire({ type: this.type.toUpperCase() }).subscribe(
-			Response => {
-				if (Response['status'] == 'success') {
-					this.questions = Response['questions'];
-					this.options = Response['options'];
-					this.group_title = Response['group_title'];
-					this.weekInfoId = Response['week_info']['id'];
-					this.short_description = Response['short_description'];
-				} else {
-					this.toastr.error(Response['msg']);
-					this.router.navigate(["/patient/dashboard"]);
-				}
-			}
-		);
-	}
+    /**
+     * @desc Saving the question's response done by patients
+     */
+    save(template?: TemplateRef<any>) {
+        const data = [],
+            option_ids = document.querySelectorAll(
+                "input[name^='option[']:checked"
+            );
+        if (
+            option_ids.length == this.totalQuestions ||
+            this.confirm_submition
+        ) {
+            for (let i = 0, opt = option_ids, len = opt.length; i < len; i++) {
+                const question_id = opt[i].getAttribute("data-question-id");
+                const group_id = opt[i].getAttribute("group-id");
+                const values = opt[i]["value"];
+                const response = opt[i].getAttribute("title");
+                data.push({
+                    options_id: values,
+                    questions_id: question_id,
+                    question_groups_id: group_id,
+                    week_info_id: this.weekInfoId,
+                    response: response
+                });
+            }
 
-	/**
-	 * @desc Saving the question's response done by patients
-	 */
-	save() {
-		const data = [],
-			option_ids = document.querySelectorAll("input[name^='option[']:checked");
-		for (let i = 0, opt = option_ids, len = opt.length; i < len; i++) {
-			const question_id = opt[i].getAttribute('data-question-id');
-			const group_id = opt[i].getAttribute('group-id');
-			const values = opt[i]['value'];
-			const response = opt[i].getAttribute('title');
-			data.push({ options_id: values, questions_id: question_id, question_groups_id: group_id, week_info_id: this.weekInfoId, response: response });
-
-		}
-
-		this.questionnaireService.save_questionnaire(data).subscribe(
-			Response => {
-				if (Response['status'] == 'success') {
-					let path: any;
-					path = this.type == 'intolerance' ? ['/questionnaire/rumination'] : 
-								(this.type == 'rumination' ? ['/questionnaire/promis'] : 
-									(this.type == 'promis' ? ['/questionnaire/fact'] :
-									 ['/patient/dashboard']));
-					this.router.navigate(path).then(() => {
-						this.toastr.success(Response['msg']);
-					});
-				} else {
-					this.toastr.error(Response['msg']);
-				}
-			}
-		);
-	}
-
-
+            this.questionnaireService
+                .save_questionnaire(data)
+                .subscribe(Response => {
+                    if (Response["status"] == "success") {
+                        let path: any;
+                        path =
+                            this.type == "intolerance"
+                                ? ["/questionnaire/rumination"]
+                                : this.type == "rumination"
+                                ? ["/questionnaire/promis"]
+                                : this.type == "promis"
+                                ? ["/questionnaire/fact"]
+                                : ["/patient/dashboard"];
+                        this.router.navigate(path).then(() => {
+                            this.toastr.success(Response["msg"]);
+                            this.confirm_submition = false;
+                            this.modalRef.hide();
+                        });
+                    } else {
+                        this.toastr.error(Response["msg"]);
+                    }
+                });
+        } else {
+            this.openModal(template);
+        }
+    }
 }
